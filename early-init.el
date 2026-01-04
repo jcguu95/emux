580622b5683a4e5081519f5bde20c19f0443eac6
jcguu95 <jcguu95@gmail.com>
2026-01-03 23:17:30 -0700

    refactor

--- early-init.el
+++ early-init.el
@@ -1,5 +1,9 @@
 ;;; -*- lexical-binding: t; -*-
 
+(setq debug-on-error t)
+;; (setq warning-minimum-level :debug)
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
 ;; 提高 GC 門檻以加速啟動
 ;; 啟動完成後調回合理的數值（這段通常留在 init.el 的最後面）
 (setq gc-cons-threshold (* 100 1024 1024))
--- init.el
+++ init.el
@@ -1,22 +1,29 @@
 ;;; -*- lexical-binding: t; -*-
 ;;; Usage: emacs --init-dir=/path/to/this/dir/
 
-(setq warning-minimum-level :debug)
-(setq debug-on-error t)
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-
-(require 'init-straight)
+;;; 
+(require 'init-straight)                ; NOTE Emacs' Package Manager
 (require 'init-benchmark)               ; NOTE 平常沒必要時，把這個刪掉以增加 booting 效率。
-(require 'init-ui)
-(require 'init-evil)
-(require 'init-eat)                     ; EAT, an emacs terminal emulator with sane LINE-MODE.
-(require 'init-bindings)
-(require 'init-window)
-(require 'init-completion)
 
-;; --- System Clipboard ---
-;; TODO Integrate `emacs -nw` and system clipboard (#'evil-yank and
-;; #'evil-paste-after). See how doomemacs did this.
 
+
+;;; Core Profile
+;; 
+;;  A high-performance, tmux-inspired Emacs environment optimized for
+;;  terminal-based workflows. It provides a lightweight "Multiplexer"
+;;  experience that prioritizes rapid startup, modal efficiency, and deep
+;;  system introspection. It takes ~170ms to boot on a Macbook M2.
+;; 
+(require 'init-core-profile)
+
+
+
+;;; Extended Profile TODO
+;; 
+;; (require 'init-extended-profile)
+
+
+
+;;; 
 (add-hook 'emacs-startup-hook
           (lambda () (setq gc-cons-threshold (* 2 1024 1024))))
