8855b8a95d29ac491485887b02b81ae069be54d4
jcguu95 <jcguu95@gmail.com>
2026-01-04 12:03:45 -0700

    Add emux.

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TARGET_DIR="$HOME/.scripts"
+BINARY_NAME="emux"
+
+mkdir -p "$TARGET_DIR"
+ln -sf "$SCRIPT_DIR/emux" "$TARGET_DIR/$BINARY_NAME"
